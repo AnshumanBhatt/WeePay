a5536727999770729d2e899fcc947fe5ae50355c
Anshuman Bhatt <94911238+AnshumanBhatt@users.noreply.github.com>
2025-07-16 15:00:33 +0530

    Supabase Auth added

--- WeePay/WeePayApp.swift
+++ WeePay/WeePayApp.swift
@@ -13,7 +13,7 @@ struct WeePayApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
